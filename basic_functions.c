aa9fc4662f06e7cc50d4b6617161129ff22377ae
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-28 15:58:11 +0530

    added lcm function

--- basic_functions.c
+++ basic_functions.c
@@ -4,6 +4,7 @@ unsigned char is_odd(int);
 int square(int);
 int cube(int);
 int gcd(int, int);
+int lcm(int, int);
 
 unsigned char is_even(int number)
 {
@@ -37,6 +38,11 @@ int gcd(int number1, int number2)
   }
 }
 
+int lcm(int number1, int number2)
+{
+  return number1 * number2 / gcd(number1, number2);
+}
+
 int main(void)
 {
   int number, number1, number2;
@@ -46,7 +52,10 @@ int main(void)
   printf("%d is %s \n", number, is_odd(number) ? "odd" : "not odd");
   printf("Square of %d is %d \n", number, square(number));
   printf("Cube of %d is %d \n", number, cube(number));
-  printf("Enter Two Numbers ");
+  printf("Enter two numbers for calculating G.C.D. ");
   scanf("%d %d", &number1, &number2);
-  printf("G.C.D. of %d and %d is %d", number1, number2, gcd(number1, number2));
+  printf("G.C.D. of %d and %d is %d \n", number1, number2, gcd(number1, number2));
+  printf("Enter Two Numbers for calculating L.C.M. ");
+  scanf("%d %d", &number1, &number2);
+  printf("L.C.M. of %d and %d is %d \n", number1, number2, lcm(number1, number2));
 }
