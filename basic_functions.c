3de6a4e3e99582d3f4e323d496747309e24ac431
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-28 15:53:50 +0530

    added gcd function

--- basic_functions.c
+++ basic_functions.c
@@ -3,6 +3,7 @@ unsigned char is_even(int);
 unsigned char is_odd(int);
 int square(int);
 int cube(int);
+int gcd(int, int);
 
 unsigned char is_even(int number)
 {
@@ -24,13 +25,28 @@ int cube(int number)
   return square(number) * number;
 }
 
+int gcd(int number1, int number2)
+{
+  if (number2 != 0)
+  {
+    return gcd(number2, number1 % number2);
+  }
+  else
+  {
+    return number1;
+  }
+}
+
 int main(void)
 {
-  int number;
+  int number, number1, number2;
   printf("Enter the number ");
   scanf("%d", &number);
   printf("%d is %s \n", number, is_even(number) ? "even" : "not even");
   printf("%d is %s \n", number, is_odd(number) ? "odd" : "not odd");
   printf("Square of %d is %d \n", number, square(number));
   printf("Cube of %d is %d \n", number, cube(number));
+  printf("Enter Two Numbers ");
+  scanf("%d %d", &number1, &number2);
+  printf("G.C.D. of %d and %d is %d", number1, number2, gcd(number1, number2));
 }
