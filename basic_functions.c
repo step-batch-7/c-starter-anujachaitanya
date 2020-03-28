19649d813a648f7eb4994f84d8a0303585a22ba8
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-28 15:33:17 +0530

    Added sqaure function and made the script to run the functions

--- basic_functions.c
+++ basic_functions.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 unsigned char is_even(int);
 unsigned char is_odd(int);
+int square(int);
 
 unsigned char is_even(int number)
 {
@@ -12,6 +13,11 @@ unsigned char is_odd(int number)
   return !is_even(number);
 }
 
+int square(int number)
+{
+  return number * number;
+}
+
 int main(void)
 {
   int number;
@@ -19,4 +25,5 @@ int main(void)
   scanf("%d", &number);
   printf("%d is %s \n", number, is_even(number) ? "even" : "not even");
   printf("%d is %s \n", number, is_odd(number) ? "odd" : "not odd");
+  printf("Square of %d is %d \n", number, square(number));
 }
