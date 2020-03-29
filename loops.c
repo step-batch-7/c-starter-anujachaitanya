01f288ffcb0a2778090a49df047e7ab21a0739f4
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-29 16:02:17 +0530

    added print_evens  function to loops

--- loops.c
+++ loops.c
@@ -2,6 +2,8 @@
 
 unsigned long int factorial(int);
 int fibonacci(int);
+int print_evens(int);
+unsigned char is_even(int);
 
 unsigned long int factorial(int number)
 {
@@ -22,6 +24,21 @@ int fibonacci(int length)
   return 0;
 }
 
+unsigned char is_even(int number)
+{
+  return number % 2 == 0;
+}
+
+int print_evens(int length)
+{
+  printf("\nEven numbers between 1 and %d are:\n", length);
+  for (int num = 1; num <= length; num++)
+  {
+    is_even(num) && printf("%d\n", num);
+  }
+  return 0;
+}
+
 int main(void)
 {
   int number;
@@ -29,8 +46,12 @@ int main(void)
   scanf("%d", &number);
   printf("Factorial of %d is %lu\n", number, factorial(number));
 
-  printf("Enter the number to get fibonacci series : ");
+  printf("\nEnter the number to get fibonacci series : ");
   scanf("%d", &number);
   fibonacci(number);
+
+  printf("\nEnter the number to get all the evens: ");
+  scanf("%d", &number);
+  print_evens(number);
   return 0;
 }
